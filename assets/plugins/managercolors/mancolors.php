01d82f65add61c40e1335e84c614d3b6b7e01e2c
Nicola <nlambathakis@gmail.com>
2025-01-14 21:18:56 +0100

    improved custom Nav logo support

--- assets/plugins/managercolors/mancolors.php
+++ assets/plugins/managercolors/mancolors.php
@@ -119,10 +119,14 @@ else {
             $stylenavlogodisplay = 'body #mainMenu [data-evocp="bgmColor"] #nav #site::before, #mainMenu #nav #site::before, body.lightness #mainMenu #nav #site::before {left: 0; top: 0.25rem; width: 100%; height: 2.0rem; background: url("media/style/default/images/misc/logo-navbar-left-white.png") no-repeat 0 50%; background-size: 2.0rem;} @media (min-width: 1200px) {
                 #mainMenu #nav #site a {margin-left:3.0rem;;} }';  
         }
-        if ($CustomLogoPath !== '' && $ShowNavLogo == 'custom') {
+        if ($CustomLogoPath !== '' && $ShowNavLogo == 'customwide') {
             $stylelogocustom = 'body #mainMenu [data-evocp="bgmColor"] #nav #site::before, #mainMenu #nav #site::before, body.lightness #mainMenu #nav #site::before {background: url("'.$CustomLogoPath.'") 0 50% no-repeat; background-size: 8rem; }';
         }
-        else {
+        if ($CustomLogoPath !== '' && $ShowNavLogo == 'customsquare') {
+            $stylelogocustom = 'body #mainMenu [data-evocp="bgmColor"] #nav #site::before, #mainMenu #nav #site::before, body.lightness #mainMenu #nav #site::before {left: 0; top: 0.25rem; width: 100%; height: 2.0rem; background: url("'.$CustomLogoPath.'") no-repeat 0 50%; background-size: 2.0rem;} @media (min-width: 1200px) {
+                #mainMenu #nav #site a {margin-left:3.0rem;;} }';
+        }
+        if ($ShowNavLogo == 'default') {
             $stylelogocustom = '';
         }
         $topcssOutput = '
